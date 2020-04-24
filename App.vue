014d38a5797736fae95ede50c5ac4a08dc56b29a
fujisawatk <fuji19880707@gmail.com>
2020-04-24 14:21:12 +0900

    create header

--- App.vue
+++ App.vue
@@ -1,18 +1,28 @@
 <template>
-  <view class="container">
-    <text-input
-      :style="{height: 40, width: 200, borderColor: 'gray', borderWidth: 1}"
-      v-model="messageText"
-    />
-    <button
-      :on-press="saveMessage"
-      title="Save"
-    />
-    <text v-for="(message, index) in messages" :key="index">{{ message }}</text>
+  <view id="app">
+    <header/>
+    <view class="container">
+      <text-input
+        :style="{height: 40, width: 200, borderColor: 'gray', borderWidth: 1}"
+        v-model="messageText"
+      />
+      <nb-button light :on-press="saveMessage"><nb-text>Light</nb-text></nb-button>
+      <text v-for="(message, index) in messages" :key="index">{{ message }}</text>
+    </view>
   </view>
 </template>
+
 <script>
+import Vue from "vue-native-core";
+import { VueNativeBase } from 'native-base';
+import header from './src/components/header';
+
+Vue.use(VueNativeBase);
+
   export default {
+    components: {
+      header
+    },
     data: function() {
       return {
         messageText: '',
