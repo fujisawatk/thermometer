a5ec2bffd2e0fa1cbe9cd65f58a37d9f77dcf37f
fujisawatk <fuji19880707@gmail.com>
2020-04-27 14:21:23 +0900

    add chart view
    チャートライブラリを"victory-native"に変更したため、"react-native-chart-kit"は削除。

--- App.vue
+++ App.vue
@@ -1,51 +1,23 @@
 <template>
-  <view id="app">
-    <header/>
-    <view class="container">
-      <text-input
-        :style="{height: 40, width: 200, borderColor: 'gray', borderWidth: 1}"
-        v-model="messageText"
-      />
-      <nb-button light :on-press="saveMessage"><nb-text>Light</nb-text></nb-button>
-      <text v-for="(message, index) in messages" :key="index">{{ message }}</text>
-    </view>
+  <view class="container">
+    <header />
+    <chart />
   </view>
 </template>
 
 <script>
-import Vue from "vue-native-core";
+import Vue from 'vue-native-core';
 import { VueNativeBase } from 'native-base';
-import header from './src/components/header';
+import Header from "./src/header.vue";
+import Chart from "./src/charts/rChart.js";
 
 Vue.use(VueNativeBase);
 
-  export default {
-    components: {
-      header
-    },
-    data: function() {
-      return {
-        messageText: '',
-        messages: []
-      }
-    },
-    methods: {
-      saveMessage: function() {
-        this.messages.push(this.messageText)
-        this.messageText = ''
-      }
-    }
-  }
-</script>
+export default {
+  components: {
+    Header,
+    Chart
+  },
+};
 
-<style>
-.container {
-  background-color: white;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-}
-.text-color-primary {
-  color: blue;
-}
-</style>
+</script>
---
+++ src/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <nb-header class="header">
+    <nb-left />
+      <nb-body>
+        <nb-title>検温アプリ</nb-title>
+      </nb-body>
+    <nb-right />
+  </nb-header>
+</template>
+
+<style>
+.header {
+ background-color: #91e4fb;
+}
+</style>
