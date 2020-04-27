b73eb5477be03cee49a4a62d36d119122648eec0
fujisawatk <fuji19880707@gmail.com>
2020-04-28 00:21:50 +0900

    add button view

---
+++ src/components/btn.vue
@@ -0,0 +1,24 @@
+<template>
+  <view class="button">
+      <nb-icon type="AntDesign" name="plus" class="plus" />
+  </view>
+</template>
+
+<style>
+.button {
+  position: relative;
+  background-color: #91e4fb;
+  width: 130px;
+  height: 130px;
+  border-radius: 65px;
+  border-style: solid;
+}
+.plus {
+  font-size: 90px;
+  position: absolute;
+  top: 20;
+  right: 0;
+  left: 20;
+  bottom: 0;
+}
+</style>
