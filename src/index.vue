ec9b4d07bc28faea220167717b027fc96a040944
fujisawatk <fuji19880707@gmail.com>
2020-04-28 13:30:51 +0900

    add modal package
    フォーム画面をモーダル表示にする為、モーダル機能を追加。

--- src/index.vue
+++ src/index.vue
@@ -1,9 +1,35 @@
 <template>
     <root class="root">
-      <header />
-      <chart />
-      <list />
-      <btn class="footer" />
+      <safe-area-view
+        class="safe-area"
+      >
+        <header />
+        <chart />
+        <list />
+
+        <modal 
+          :isVisible="isModalVisible"
+        >
+          <view class="modal">
+              <text>Modal Content</text>
+              <button
+                  title="Close modal"
+                  :on-press="toggleModal"
+              />
+          </view>
+        </modal>
+    
+        <!-- <btn
+          class="footer"
+          :on-press="toggleModal"
+        /> -->
+        <view class="footer">
+          <view class="button">
+            <nb-icon type="AntDesign" name="plus" class="plus" :on-press="toggleModal" />
+          </view>
+        </view>
+
+      </safe-area-view>
     </root>
 </template>
 
@@ -13,7 +39,11 @@ import { Root } from "native-base";
 import Header from "./components/header.vue";
 import Chart from "./components/rChart.js";
 import List from "./components/list.vue";
-import Btn from "./components/btn.vue";
+// import Btn from "./components/btn.vue";
+import { SafeAreaView } from 'react-native';
+import Modal from "react-native-modal";
+
+
 
 export default {
   components: {
@@ -21,7 +51,24 @@ export default {
     Header, 
     Chart, 
     List,
-    Btn
+    // Btn,
+    SafeAreaView,
+    Modal
+  },
+  data: function() {
+    return {
+      state: false
+    }
+  },
+  computed: {
+    isModalVisible: function() {
+      return this.state
+    }
+  },
+  methods: {
+    toggleModal: function() {
+      return this.state = !this.state
+    }
   }
 };
 </script> 
@@ -36,4 +83,32 @@ export default {
   right: 20;
   bottom: 20;
 }
+
+.button {
+  position: relative;
+  background-color: #91e4fb;
+  width: 130px;
+  height: 130px;
+  border-radius: 65px;
+  border-style: solid;
+}
+.plus {
+  font-size: 90px;
+  position: absolute;
+  top: 20;
+  right: 0;
+  left: 20;
+  bottom: 0;
+}
+.safe-area {
+  flex: 1;
+}
+
+.modal {
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+}
+
 </style>
