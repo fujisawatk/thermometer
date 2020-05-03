15e5aa88f9683fb688c83996fd552b018f84cfd1
fujisawatk <fuji19880707@gmail.com>
2020-05-03 13:22:08 +0900

    add a font setting function
    Android対応による、フォント読み込みエラー対策

--- src/setup.vue
+++ src/setup.vue
@@ -1,6 +1,7 @@
 <template>
   <view class="container">
-    <app />
+    <app-loading v-if="!isAppReady" />
+    <app v-else />
   </view>
 </template>
 
@@ -8,14 +9,36 @@
 import Vue from 'vue-native-core';
 import { VueNativeBase } from 'native-base';
 import App from "./index.vue";
+import { AppLoading } from 'expo';
 
 
 Vue.use(VueNativeBase);
 
 export default {
-  components: {
-    App
-  }
+  components: { App, AppLoading },
+  data: function() {
+    return {
+      isAppReady: false,
+    };
+  },
+  created: function() {
+    this.loadFonts();
+  },
+  methods: {
+    loadFonts: async function() {
+      try {
+        this.isAppReady = false;
+        await Expo.Font.loadAsync({
+          Roboto: require('native-base/Fonts/Roboto.ttf'),
+          Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+        });
+        this.isAppReady = true;
+      } catch (error) {
+        console.log('some error occured', error);
+        this.isAppReady = true;
+      }
+    },
+  },
 };
 </script>
 
