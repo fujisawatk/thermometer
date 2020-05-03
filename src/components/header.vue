c1fab8ef70026558e586a328e664a17943fd8a38
fujisawatk <fuji19880707@gmail.com>
2020-05-03 15:01:59 +0900

    fix header style
    アプリタイトルがOSによって位置が違ったため、style調整。

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,10 +1,10 @@
 <template>
   <nb-header class="header">
-    <nb-left />
-      <nb-body>
-        <nb-title class="header-title">検温アプリ</nb-title>
-      </nb-body>
-    <nb-right>
+    <nb-left class="header-left"/>
+    <nb-body class="header-body">
+      <nb-title class="header-title">検温アプリ</nb-title>
+    </nb-body>
+    <nb-right class="header-right">
       <nb-button transparent>
         <nb-icon class="medical-icon" type="FontAwesome5" name="hospital-alt" :on-press="openLink" />
       </nb-button>
@@ -20,7 +20,9 @@ export default {
       openLink: function() {
         Linking
           .openURL("https://www.mhlw.go.jp/stf/seisakunitsuite/bunya/kenkou_iryou/dengue_fever_qa_00001.html")
-          .catch(err => console.error('URLを開けませんでした。', err));
+          .catch(
+            err => console.error('URLを開けませんでした。', err)
+          );
       }
   },
 };
@@ -28,11 +30,24 @@ export default {
 
 <style>
 .header {
- background-color: #91e4fb;
+  flex-direction: row;
+  background-color: #91e4fb;
+}
+
+.header-left,
+.header-right{
+  flex:1;
+}
+
+.header-body {
+  flex: 1;
+  align-items: center;
 }
 .header-title {
-  font-size: 23;
+  color: black;
+  font-weight: bold;
 }
+
 .medical-icon {
   color: black;
 }
